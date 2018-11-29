10d1199ac15e5ad9295d6d873e82e528d29f6812
894923712 <894923712@qq.com>
2018-11-29 23:44:37 +0800

    Initial commit

---
+++ mylm/src/main/java/com/lm/mylm/MylmApplication.java
@@ -0,0 +1,12 @@
+package com.lm.mylm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylmApplication.class, args);
+	}
+}
---
+++ mylm/src/test/java/com/lm/mylm/MylmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lm.mylm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MylmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
